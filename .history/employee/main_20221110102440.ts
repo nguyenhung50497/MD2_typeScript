44276e0536eaf6d7c8c2cf540a015056dccafdde
hungnguyen50497 <nguyenhung50497@gmail.com>
2022-11-10 10:27:33 +0700

    new

---
+++ .history/employee/main_20221110102440.ts
@@ -0,0 +1,12 @@
+enum Gender {
+
+    MALE,
+  
+  
+    FEMALE,
+  
+  
+    OTHER
+  
+  
+  }
---
+++ .history/employee/main_20221110102516.ts
@@ -0,0 +1,32 @@
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER 
+  }
+  class Employee {
+
+    fullname: string = "";
+  
+  
+    gender: Gender = Gender.OTHER;
+  
+  
+    birthday?: Date;
+  
+  
+    email: string = "";
+  
+  
+    phoneNumber: string = "";
+  
+   
+  
+  
+    constructor(fullname: string, gender: Gender, birthday: Date, email: string, phoneNumber: string) {
+      this.fullname = fullname;
+      this.gender = gender;
+      this.birthday = birthday;
+      this.email = email;
+      this.fullname = fullname;
+      this.phoneNumber = phoneNumber;
+    }
